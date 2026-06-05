bc60fb072954bd9ea2a15a89c1a44c2cf11be9b1
Eli G <104237829+jemiamapus@users.noreply.github.com>
2026-06-05 16:46:49 -0400

    Add bin sorting

--- comp_analysis_modified.py
+++ comp_analysis_modified.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 import time
+import textwrap
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -623,6 +624,11 @@ for i, board in enumerate(result_id[:, 0]):   # loop over boards
         #print(board_letter)
         # skip empty / bad values if needed
         
+        bins = [5.006, 5.018, 5.031, 5.043, 5.056, 5.068, 5.081, 5.094, 5.106, 5.118, 5.131, 5.143, 5.156, 5.168, 5.181, 5.193, 5.206, 5.218, 5.231] # change this if new bins are added
+        bin_num = len(list(filter(lambda r: r+0.0005 < resistance, bins)))
+        bin_names = textwrap.wrap('NQNPNONNNMNLNKNINHNGNFNENDNCNBNAOOPAPBPT', 2) # lazy hack so i didn't have to type out a bunch of commas
+        res_bin = bin_names[bin_num]
+        
         labels.append({
             "module": module_number,
             "board": board_letter,  # optional cleanup
@@ -630,7 +636,7 @@ for i, board in enumerate(result_id[:, 0]):   # loop over boards
             "channel": ch + 1,
             "date": date_str,
              "resistance": f"{resistance:.3f} GΩ".encode("utf-8") if 3 <= resistance <= 6 else "nan", # .encode() is necessary on windows
-            "Bin": "_____"
+            "Bin": res_bin
         })
 
 # =========================
