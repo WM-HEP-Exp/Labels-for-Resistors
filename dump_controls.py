86efe03c758ea791f8cbd33cd50afd178058a7a1
Dune Testing <rnjames@wm.edu>
2026-06-22 18:22:46 -0400

    dump_controls.py additions

--- dump_controls.py
+++ dump_controls.py
@@ -11,14 +11,43 @@ def enumerate_folders(directory):
     return data_folders
 
 def get_resistances(data_folder):
-    p1 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M1", "1", "XX-XX-XXXX", __file__ + os.sep + "CalFilesCB1"], encoding="utf-8", capture_output=True)
+    p1 = subprocess.run([sys.executable, os.path.dirname(__file__) + os.sep + "comp_analysis_modified.py", data_folder+"/M1", "1", "XX-XX-XXXX", os.path.dirname(__file__) + os.sep + "CalFilesCB1"], encoding="utf-8", capture_output=True)
     sys.stderr.write(p1.stderr)
     sys.stderr.flush()
-    p2 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M2", "2", "XX-XX-XXXX", __file__ + os.sep + "CalFilesCB2"], encoding="utf-8", capture_output=True)
+    p2 = subprocess.run([sys.executable, os.path.dirname(__file__) + os.sep + "comp_analysis_modified.py", data_folder+"/M2", "2", "XX-XX-XXXX", os.path.dirname(__file__) + os.sep + "CalFilesCB2"], encoding="utf-8", capture_output=True)
     sys.stderr.write(p2.stderr)
     sys.stderr.flush()
-    p3 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M3", "3", "XX-XX-XXXX", __file__ + os.sep + "CalFilesCB3"], encoding="utf-8", capture_output=True)
+    p3 = subprocess.run([sys.executable, os.path.dirname(__file__) + os.sep + "comp_analysis_modified.py", data_folder+"/M3", "3", "XX-XX-XXXX", os.path.dirname(__file__) + os.sep + "CalFilesCB3"], encoding="utf-8", capture_output=True)
     sys.stderr.write(p3.stderr)
     sys.stderr.flush()
     all_resistances = ast.literal_eval('[\n'+p1.stdout+p2.stdout+p3.stdout+'\n]') # similar to JSON.load, but handles trailing commas without crashing
     return all_resistances
+
+def map_resistances(resistances):
+    resistance_map = {}
+    for i in resistances:
+        resistance_map[str(i['module']) + '_' + str(i['board']) + '_' + str(i['channel'])] = i['resistance'].decode('utf-8')
+    return resistance_map
+
+def filter_resistances(resistance_map):
+    key1 = '1_A_8'
+    key2 = '2_A_1'
+    key3 = '3_A_1'
+    res1 = resistance_map[key1] if key1 in resistance_map else 'nan'
+    res1 = resistance_map[key2] if key2 in resistance_map else 'nan'
+    res1 = resistance_map[key3] if key3 in resistance_map else 'nan'
+    return [str(key1), str(key2, key3]
+
+if __name__ == '__main__':
+    #folder_map = {}
+    folders = enumerate_folders(sys.argv[1])
+    for f in folders:
+        print(f, file=sys.stderr)
+        key = os.path.split(f.rstrip('/'))[1] # returns date, such as "6-22-26_run2"
+        try:
+            value = filter_resistances(map_resistances(get_resistances(f)))
+            print(key + ": " + str(value))
+        except SyntaxError:
+            continue
+        #folder_map[key] = value
+    #print(folder_map)
