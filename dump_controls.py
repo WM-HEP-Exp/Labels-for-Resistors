e1a94585b6dabc29ca4bb1647850576d506828ab
Eli G <104237829+jemiamapus@users.noreply.github.com>
2026-06-22 17:38:08 -0400

    add calibration support

--- dump_controls.py
+++ dump_controls.py
@@ -11,8 +11,8 @@ def enumerate_folders(directory):
     return data_folders
 
 def get_resistances(data_folder):
-    p1 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M1", "1", "XX-XX-XXXX"], encoding="utf-8", capture_output=True)
-    p2 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M2", "2", "XX-XX-XXXX"], encoding="utf-8", capture_output=True)
-    p3 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M3", "3", "XX-XX-XXXX"], encoding="utf-8", capture_output=True)
+    p1 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M1", "1", "XX-XX-XXXX", __file__ + os.sep + "CalFilesCB1"], encoding="utf-8", capture_output=True)
+    p2 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M2", "2", "XX-XX-XXXX", __file__ + os.sep + "CalFilesCB2"], encoding="utf-8", capture_output=True)
+    p3 = subprocess.run([sys.executable, __file__ + os.sep + "comp_analysis_modified.py", data_folder+"/M3", "3", "XX-XX-XXXX", __file__ + os.sep + "CalFilesCB3"], encoding="utf-8", capture_output=True)
     all_resistances = ast.literal_eval('[\n'+p1.stdout+p2.stdout+p3.stdout+'\n]') # similar to JSON.load, but handles trailing commas without crashing
     return all_resistances
