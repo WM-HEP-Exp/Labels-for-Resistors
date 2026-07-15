4b0c8e256ce36fbc25f48ac2b0fde7de6049dbbc
Brandon <bdstobie@wm.edu>
2026-07-15 16:50:09 -0400

    Added fallback for missing module data

--- gen_pdf.py
+++ gen_pdf.py
@@ -21,24 +21,33 @@ headertext = header.read()
 header.close()
 f.write(headertext)
 print("running modules...")
-p1 = subprocess.run([sys.executable, "comp_analysis_modified.py", filepath+"/M1", "1", filedate], encoding="utf-8", capture_output=True)
-f.write(p1.stdout)
-sys.stderr.write(p1.stderr) # debugging
-#sys.stderr.write(os.linesep)
-sys.stderr.flush()
-print("mod 1 done")
-p2 = subprocess.run([sys.executable, "comp_analysis_modified.py", filepath+"/M2", "2", filedate], encoding="utf-8", capture_output=True)
-f.write(p2.stdout)
-sys.stderr.write(p2.stderr) # debugging
-#sys.stderr.write(os.linesep)
-sys.stderr.flush()
-print("mod 2 done")
-p3 = subprocess.run([sys.executable, "comp_analysis_modified.py", filepath+"/M3", "3", filedate], encoding="utf-8", capture_output=True)
-f.write(p3.stdout)
-sys.stderr.write(p3.stderr) # debugging
-#sys.stderr.write(os.linesep)
-sys.stderr.flush()
-print("mod 3 done")
+p1 = subprocess.run(["python3", "comp_analysis_modified.py", sys.argv[1]+"/M1", "1", sys.argv[2]], encoding="utf-8", capture_output=True)
+if p1.stdout != "No data files found, exiting\n":
+	f.write(p1.stdout)
+	#sys.stderr.write(p1.stderr) # debugging
+	#sys.stderr.write(os.linesep)
+	#sys.stderr.flush()
+	print("mod 1 done")
+else:
+	print("mod 1 no data files")
+p2 = subprocess.run(["python3", "comp_analysis_modified.py", sys.argv[1]+"/M2", "2", sys.argv[2]], encoding="utf-8", capture_output=True)
+if p2.stdout != "No data files found, exiting\n":
+	f.write(p2.stdout)
+	#sys.stderr.write(p2.stderr) # debugging
+	#sys.stderr.write(os.linesep)
+	#sys.stderr.flush()
+	print("mod 2 done")
+else:
+	print("mod 2 no data files")
+p3 = subprocess.run(["python3", "comp_analysis_modified.py", sys.argv[1]+"/M3", "3", sys.argv[2]], encoding="utf-8", capture_output=True)
+if p3.stdout != "No data files found, exiting\n":
+	f.write(p3.stdout)
+	#sys.stderr.write(p3.stderr) # debugging
+	#sys.stderr.write(os.linesep)
+	#sys.stderr.flush()
+	print("mod 3 done")
+else:
+	print("mod 3 no data files")
 footer = open("Labels-footer.txt", "r")
 footertext = footer.read()
 footer.close()
